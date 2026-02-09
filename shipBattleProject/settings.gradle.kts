63e642cebdee3a4a57bd2fdf04c6fc06027ffe69
wombatriy <drakot88@gmail.com>
2026-02-10 00:35:07 +0300

    feat: removed tooolchains

--- shipBattleProject/settings.gradle.kts
+++ shipBattleProject/settings.gradle.kts
@@ -20,10 +20,6 @@ pluginManagement {
     }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 include(":shipBattle")
